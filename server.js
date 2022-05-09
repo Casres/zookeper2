82c6cc873f3e359d43c17e582692aa11583bf63b
Casres <christian@casillas.tech>
2022-05-08 23:19:08 -0400

    Fixed my error, named the array inccorectly

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-
-
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const { animals } = require("./data/animals.json");
 
 // starts the server
 const express = require("express");
@@ -13,31 +12,16 @@ app.use(express.urlencoded({ extended: true }));
 // this parses incoming data
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!!`);
-});
-
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   let filteredResults = animalsArray;
-
   if (query.personalityTraits) {
-    // Save personalityTraits as a dedicated array.
-    // If personalityTraits is a string, place it into a new array and save.
     if (typeof query.personalityTraits === "string") {
       personalityTraitsArray = [query.personalityTraits];
     } else {
       personalityTraitsArray = query.personalityTraits;
     }
-    // Loop through each trait in the personalityTraits array:
     personalityTraitsArray.forEach((trait) => {
-      // Check the trait against each animal in the filteredResults array.
-      // Remember, it is initially a copy of the animalsArray,
-      // but here we're updating it for each trait in the .forEach() loop.
-      // For each trait being targeted by the filter, the filteredResults
-      // array will then contain only the entries that contain the trait,
-      // so at the end we'll have an array of animals that have every one
-      // of the traits when the .forEach() loop is finished.
       filteredResults = filteredResults.filter(
         (animal) => animal.personalityTraits.indexOf(trait) !== -1
       );
@@ -62,22 +46,20 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
-// here we are adding to the server
-
 function findById(id, animalsArray) {
   const result = animalsArray.filter((animal) => animal.id === id)[0];
   return result;
 }
 
 function createNewAnimal(body, animalsArray) {
-const animal = body;
+  const animal = body;
   animalsArray.push(animal);
   fs.writeFileSync(
-    path.join(__dirname, './data/animals.json'),
+    path.join(__dirname, "./data/animals"),
     JSON.stringify({ animalsArray }, null, 2)
   );
   return animal;
-};
+}
 
 app.get("/api/animals/:id", (req, res) => {
   const result = findById(req.params.id, animals);
@@ -88,7 +70,6 @@ app.get("/api/animals/:id", (req, res) => {
   }
 });
 
-const { animals } = require("./data/animals.json");
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -97,6 +78,7 @@ app.get("/api/animals", (req, res) => {
   res.json(results);
 });
 
+// here we are adding to the server
 app.post("/api/animals", (req, res) => {
   // req.body is where our incoming content will be
   console.log(req.body, "Hi mom!");
@@ -104,7 +86,7 @@ app.post("/api/animals", (req, res) => {
   req.body.id = animals.length.toString();
 
   if (!validateAnimal(req.body)) {
-    res.status(400).send('The animal is not properly formatted');
+    res.status(400).send("The animal is not properly formatted");
   } else {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
@@ -112,17 +94,24 @@ app.post("/api/animals", (req, res) => {
 });
 
 function validateAnimal(animal) {
-  if (!animal.name || typeof animal.name !== 'string') {
+  if (!animal.name || typeof animal.name !== "string") {
     return false;
   }
-  if (!animal.species || typeof animal.species !== 'string') {
+  if (!animal.species || typeof animal.species !== "string") {
     return false;
   }
-  if (!animal.diet || typeof animal.diet !== 'string') {
+  if (!animal.diet || typeof animal.diet !== "string") {
     return false;
   }
-  if (!animal.personalityTraits || typeof animal.personalityTraits !== 'string') {
+  if (
+    !animal.personalityTraits ||
+    typeof animal.personalityTraits !== "string"
+  ) {
     return false;
   }
   return true;
-};
+}
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!!`);
+});
