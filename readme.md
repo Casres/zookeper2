291c60f5c196d252cba38d13660421bd6ec34de4
Casres <christian@casillas.tech>
2022-05-08 20:57:51 -0400

    AskBCS SP/ GitHub save

---
+++ readme.md
@@ -0,0 +1,47 @@
+
+# **test**
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## **Table of contents**
+- [Description](#Description)
+- [Languages](#Languages)
+- [Usage](#Usage)
+- [Installation](#Installation)
+- [Contact Information](#Contact)
+- [FAQ](#FAQ)
+- [How to contribute](#How-to-contribute)
+
+## **Description**
+test
+
+## **Languages**
+-  JavaScript, HTML, CSS, ES6, jQuery, Bootstrap, Node, Next.js, React.js, Vue.js, Angular, Svelte, Laravel, Other 
+
+
+## **License**
+This project is licensed under: MIT
+
+## **Usage**
+test
+
+## **Installation**
+
+How to install: 
+test
+
+## **Contact**
+
+#### GitHub profile
+[test](https://github.com/test)
+
+#### Reach Out
+test
+
+## **FAQ**
+test
+
+## **How-to-contribute**
+Submit issues with your code to the repository and I'll be sure to check them out 
+test
+    
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+
+
+const fs = require('fs');
+const path = require('path');
+
 // starts the server
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// this parses incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// this parses incoming data
+app.use(express.json());
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!!`);
 });
@@ -58,12 +69,22 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animalsArray }, null, 2)
+  );
+  return animal;
+};
+
 app.get("/api/animals/:id", (req, res) => {
   const result = findById(req.params.id, animals);
   if (result) {
     res.json(result);
   } else {
-    res.sendStatus(404, 'error');
+    res.sendStatus(404, "error");
   }
 });
 
@@ -77,7 +98,33 @@ app.get("/api/animals", (req, res) => {
 });
 
 app.post('/api/animals', (req, res) => {
-    // req.body is where our incoming content wil be
-    console.log(req.body);
-    res.json(req.body);
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
 });
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+
+  if (!animal.personalityTraits || typeof animal.personalityTraits !== 'string') {
+    return false;
+  }
+  return true;
+};
