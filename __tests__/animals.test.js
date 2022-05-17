42907f67caac7f8f7f811db4f165192c73e5f5d2
Casres <christian@casillas.tech>
2022-05-17 16:23:53 -0400

    AskBCS SP/ tests not working properly

---
+++ __tests__/animals.test.js
@@ -0,0 +1,91 @@
+
+
+
+
+
+
+const fs = require('fs');
+const {
+    filterByQuery,
+    findById,
+    creatNewAnimal,
+    validateAnimal,
+} = require('path');
+
+const {animals} = require('../data/animals.json');
+
+test("creates an animal object", () => {
+    const animal = createNewAnimal(
+        {name: "Darlene", id: "12345"}, 
+        animals
+    );
+    expect(animal.name).toBe("Darlene");
+    expect(animal.id).toBe("12345");
+});
+
+test("filters by query", () => {
+    const startingAnimals = [
+        {
+            id: 3,
+            name: "Erica",
+            species: "gorilla",
+            diet: "omnivore",
+            personalityTraits: ['quirky', 'rash']
+        },
+        {
+            id: 4, 
+            name: "Noel",
+            species: "bear",
+            diet: "carnivore",
+            personalityTraits: ['impish', 'sassy', 'brave']
+        }
+    ];
+    const updateAnimals = filterByQuery({species: "gorilla"}, startingAnimals);
+
+    expect(updateAnimals.length).toEqual(1);
+});
+
+test("finds by id", () => {
+    const  startingAnimals = [
+        {
+            id: 3,
+            name: "Erica",
+            species: "gorilla",
+            diet: "omnivore",
+            personalityTraits: ['quirky', 'rash']
+        },
+        {
+            id: 4, 
+            name: "Noel",
+            species: "bear",
+            diet: "carnivore",
+            personalityTraits: ['impish', 'sassy', 'brave']
+        }
+    ];
+    const result = findById("3", straightAnimals);
+
+    expect(result.name).toBe("Erica");
+});
+
+test("validates personality trait", () => {
+    const animal = {
+        id: "3",
+        name: "Erica",
+        species: "gorilla",
+        diet: "omnivore",
+        personalityTraits: ["quirky", "rash"]
+    };
+
+    const invalidAnimal = {
+        id: "3",
+        name: "Erica",
+        species: "gorilla",
+        diet: 'omnivore'
+    };
+
+    const result = validateAnimal(animal);
+    const result2 = validateAnimal(invalidAnimal);
+
+    expect(result).toBe(true);
+    expect(result2).toBe(false);
+});
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -7,19 +7,19 @@ const router = require("express").Router();
 
 // this is going to be the main page
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./index.html"));
+  res.sendFile(path.join(__dirname, "../../index.html"));
 });
 
 router.get("/animals", (req, res) => {
-  res.sendFile(path.join(__dirname, "./animals.html"));
+  res.sendFile(path.join(__dirname, "../../animals.html"));
 });
 
 router.get("/zookeepers", (req, res) => {
-  res.sendFile(path.join(__dirname, "./zookeepers.html"));
+  res.sendFile(path.join(__dirname, "../../zookeepers.html"));
 });
 
 router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./index.html"));
+  res.sendFile(path.join(__dirname, "../../index.html"));
 });
 
 module.exports = router;
